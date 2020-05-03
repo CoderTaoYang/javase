1db178253ca05afabbd0f169d875681e0ec86489
CoderYT <1767915413@qq.com>
2020-05-03 23:47:45 +0800

    流程控制-continue语句-基本知识

---
+++ src/com/yangtao/www/class3_2/ContinueDemo.java
@@ -0,0 +1,23 @@
+package com.yangtao.www.class3_2;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/3
+ **/
+/*
+把100~150之间不能被3整除的数输出：
+
+	continue:跳出本次循环
+*/
+public class ContinueDemo{
+
+    public static void main(String[] args){
+
+        for(int i = 100;i<150;i++){
+            if(i%3==0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
