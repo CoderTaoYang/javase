17f6ace1fb05f374b97c0741cb7fb58e3e6f3f18
CoderYT <1767915413@qq.com>
2020-05-04 22:38:36 +0800

    数组-命令参数-基本演示

---
+++ src/com/yangtao/www/class3_3/ArgsDemo.java
@@ -0,0 +1,14 @@
+package com.yangtao.www.class3_3;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/4
+ **/
+public class ArgsDemo{
+
+    public static void main(String[] args){
+        for(int i = 0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
