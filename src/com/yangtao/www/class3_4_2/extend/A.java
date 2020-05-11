0cb24c7c4c609cefa2d9191af0ebc73111d8dc91
CoderYT <1767915413@qq.com>
2020-05-11 22:44:49 +0800

    面向对象-继承-单继承

---
+++ src/com/yangtao/www/class3_4_2/extend/A.java
@@ -0,0 +1,17 @@
+package com.yangtao.www.class3_4_2.extend;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/11
+ **/
+public class A {
+
+    public void test(){
+        System.out.println("A....test()");
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
