602c56cf4a7bf8cc36ec2f670a5f7ad137256ce1
CoderYT <1767915413@qq.com>
2020-05-11 22:45:02 +0800

    面向对象-继承-单继承

---
+++ src/com/yangtao/www/class3_4_2/extend/B.java
@@ -0,0 +1,12 @@
+package com.yangtao.www.class3_4_2.extend;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/11
+ **/
+public class B {
+
+    public void test(){
+        System.out.println("B...test()");
+    }
+}
