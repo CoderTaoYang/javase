a3e9f6148623f284a4a368f9546f1cb3ee350372
CoderYT <1767915413@qq.com>
2020-05-11 22:45:14 +0800

    面向对象-继承-单继承

---
+++ src/com/yangtao/www/class3_4_2/extend/C.java
@@ -0,0 +1,14 @@
+package com.yangtao.www.class3_4_2.extend;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/11
+ **/
+public class C extends A {
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.test();
+//        Test2 test2 = new Test2();
+    }
+}
