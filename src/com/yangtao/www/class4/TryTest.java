ed501ca8c4558592d162973789753700fd114c69
CoderYT <1767915413@qq.com>
2020-05-14 19:52:32 +0800

    面向对象-异常-try/catch捕获异常

---
+++ src/com/yangtao/www/class4/TryTest.java
@@ -0,0 +1,32 @@
+package com.yangtao.www.class4;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/14
+ **/
+public class TryTest{
+    public static void main(String[] args){
+        System.out.println(test().num);
+    }
+
+    private static Num test(){
+        Num number = new Num();
+        try{
+            System.out.println("try");
+            return number;
+        }catch(Exception e){
+            System.out.println("error");
+        }finally{
+            if (number.num > 20){
+                System.out.println("number.num>20 : " + number.num);
+            }
+            System.out.println("finally");
+            number.num = 100;
+        }
+        return number;
+    }
+}
+
+class Num{
+    public int num = 10;
+}
