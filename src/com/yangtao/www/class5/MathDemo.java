51f9574bc53024cec13f9466e8fbdd1be04ac203
CoderYT <1767915413@qq.com>
2020-05-15 20:15:27 +0800

    常用类-Math类

---
+++ src/com/yangtao/www/class5/MathDemo.java
@@ -0,0 +1,16 @@
+package com.yangtao.www.class5;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/15
+ **/
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-1));
+        System.out.println(Math.sqrt(2));
+        System.out.println(Math.ceil(-3.14));
+        System.out.println(Math.floor(-3.14));
+        System.out.println(Math.pow(2,3));
+
+    }
+}
