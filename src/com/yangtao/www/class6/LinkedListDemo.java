d2feaba040e8d212e079189cdb32c0840708679e
CoderYT <1767915413@qq.com>
2020-05-16 10:01:27 +0800

    集合-LinkedList实现类-常用方法

---
+++ src/com/yangtao/www/class6/LinkedListDemo.java
@@ -0,0 +1,32 @@
+package com.yangtao.www.class6;
+
+import java.util.LinkedList;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/16
+ **/
+/*
+ * linkedList拥有更加丰富的方法实，需要用的时候查询api即可，不需要记忆
+ *
+ * */
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(123);
+        linkedList.add(false);
+        linkedList.add("abc");
+        System.out.println(linkedList);
+        linkedList.add(2,"mashibing");
+        System.out.println(linkedList);
+        linkedList.addFirst("1111");
+        System.out.println(linkedList);
+        linkedList.addLast("2222");
+        System.out.println(linkedList);
+        System.out.println(linkedList.element());
+        linkedList.offer("3333");
+        System.out.println(linkedList);
+
+
+    }
+}
