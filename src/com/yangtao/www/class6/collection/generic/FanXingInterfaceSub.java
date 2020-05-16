b15a8fab2a189b4c534e2b4f1c39ee4c53818f2f
CoderYT <1767915413@qq.com>
2020-05-16 11:01:49 +0800

    集合-泛型-泛型接口实现类

---
+++ src/com/yangtao/www/class6/collection/generic/FanXingInterfaceSub.java
@@ -0,0 +1,19 @@
+package com.yangtao.www.class6.collection.generic;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/16
+ **/
+public  class   FanXingInterfaceSub implements FanXingInterface<String> {
+
+
+    @Override
+    public String test() {
+        return null;
+    }
+
+    @Override
+    public void test2(String string) {
+
+    }
+}
