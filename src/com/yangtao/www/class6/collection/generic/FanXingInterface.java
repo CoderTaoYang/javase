5c583e58ddc69d58924c4260e4e1766f68c591ce
CoderYT <1767915413@qq.com>
2020-05-16 11:01:12 +0800

    集合-泛型-泛型接口

---
+++ src/com/yangtao/www/class6/collection/generic/FanXingInterface.java
@@ -0,0 +1,12 @@
+package com.yangtao.www.class6.collection.generic;
+
+/**
+ * @author CoderYT
+ * @date 2020/5/16
+ **/
+public interface FanXingInterface<B> {
+
+    public B test();
+
+    public void test2(B b);
+}
